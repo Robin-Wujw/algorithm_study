8524d3fd680efa898d407fc54eda5c8a30f1d6b6
Robin-Wujw <Robin.Wujw@gmail.com>
2018-10-23 21:05:51 +0800

    作业

---
+++ class_task.py
@@ -0,0 +1,55 @@
+class Solution():
+
+    def First(self,x,n):
+        b = x
+        if (n<=0):
+            return 1
+        else:
+            for i in range(n-1):
+                x = x * b
+        return x
+    def Second(self,x,n):
+         if n <=0:
+             return 1
+         x = x * self.Second(x,n-1)
+         return x
+    def Third(self,x,n):
+        b = x
+        if n <=0:
+            return 1
+        elif n == 1:
+            return x
+        elif n % 2 == 0:
+            for i in range(int(n/2)-1):
+                x = b * x
+            x = x * x
+        else:
+            for i in range((int((n-1)/2)-1)):
+                x = b*x
+            x = x * x * b
+        return x
+    def Four(self,x,n):
+        b = x
+        if n <=0:
+            return 1
+        elif n == 1:
+            return x
+        elif n % 2 == 0:
+            for i in range(int(n/2)-1):
+                x = b * x
+            x = pow(x,2)
+        else:
+            for i in range((int((n-1)/2)-1)):
+                x = b*x
+            x = pow(x,2)*b
+        return x
+
+def main():
+    a = Solution()
+    x = int(input("请输入底数："))
+    n = int(input("请输入指数："))
+    print(a.First(x,n))
+    print(a.Second(x, n))
+    print(a.Third(x, n))
+    print(a.Four(x, n))
+main()
